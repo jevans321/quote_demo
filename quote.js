82e4e58cff9d2c764bd7c863e6876418afd238ab
J <alright@Macintosh.local>
2014-11-25 21:15:00 -0800

    added button and bootstrap

--- quote.js
+++ quote.js
@@ -1,13 +1,20 @@
 $(document).ready(function(){
-	//alert("hello world");
-	var quote = "qwerty!"
+
+	var clickHandler = function(){
+
+		$.get("https://api.github.com/zen", fetchApi)
+	}
+
+	$("#quote").click(function(){
+		alert("button clicked!");
+	})
 	//"api": application programmer interface web 2.0. "provider and consumer, provider defines the api"
+	// event is response from web server
 	var fetchApi = function(data){
-		alert(quote)
 		$(".quote").text(data)
 	}
 
-	$.get("https://api.github.com/zen", fetchApi)
+	
 	//$.get("https://tranquil-headland-8146.herokuapp.com/zen.json", fetchApi)
 
 	
