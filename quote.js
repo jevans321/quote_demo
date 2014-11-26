bc0a8c467c587d4b81c8735206d7f2786206a974
J <alright@Macintosh.local>
2014-11-25 20:32:06 -0800

    fetch quote from zen api

--- quote.js
+++ quote.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
-	alert("hello world");
+	//alert("hello world");
+	var quote = "qwerty!"
+	//"api": application programmer interface web 2.0. "provider and consumer, provider defines the api"
+	var fetchApi = function(data){
+		alert(quote)
+		$(".quote").text(data)
+	}
+
+	$.get("https://api.github.com/zen", fetchApi)
+	//$.get("https://tranquil-headland-8146.herokuapp.com/zen.json", fetchApi)
+
+	
 });
